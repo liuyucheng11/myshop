2bf1c746557ceded37714eeb2be6143339b9b0c9
yucheng.liu <yucheng.liu@ucarinc.com>
2019-10-31 23:31:13 +0800

    没人性

--- src/main/java/com/my/shop/config/datasource/DBContextHolder.java
+++ src/main/java/com/my/shop/config/datasource/DBContextHolder.java
@@ -1,9 +1,13 @@
 package com.my.shop.config.datasource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DBContextHolder {
 
+    private static final Logger logger = LoggerFactory.getLogger(DBContextHolder.class);
     private static final ThreadLocal<DataSourceType> contextHolder = new ThreadLocal<>();
 
     private static final AtomicInteger counter = new AtomicInteger(-1);
@@ -32,13 +36,16 @@ public class DBContextHolder {
         }
         if (index == 0) {
             set(DataSourceType.SLAVE1);
-            System.out.println("切换到slave1");
-        }else if(index == 1) {
+            logger.info("【{}】", "切换到slave1");
+            //System.out.println("切换到slave1");
+        } else if (index == 1) {
             set(DataSourceType.SLAVE2);
-            System.out.println("切换到slave2");
-        } else{
+            logger.info("【{}】", "切换到slave2");
+            // System.out.println("切换到slave2");
+        } else {
             set(DataSourceType.SLAVE3);
-            System.out.println("切换到slave3");
+            logger.info("【{}】", "切换到slave3");
+            // System.out.println("切换到slave3");
         }
     }
 
